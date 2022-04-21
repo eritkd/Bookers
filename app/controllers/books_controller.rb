e3f8a893ab7aab5697c582b1f7543289c77f4d00
eritkd <102778281+eritkd@users.noreply.github.com>
2022-04-21 06:25:32 +0000

    [Finish] 3-課題　アプリケーションを作成してみよう：基礎編

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -29,12 +29,11 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
-    flash[:notice] = 'Book was successfully created.'
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+     flash[:notice] = 'Book was successfully created.'
+     redirect_to book_path(@book.id)
     else
-      @book = Book.find(params[:id])
       render :edit
     end
   end
